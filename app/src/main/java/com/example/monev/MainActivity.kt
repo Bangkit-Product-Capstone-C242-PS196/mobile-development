0bb50299cba64ff332a1ca2543815803b80f3e91
ahmadazizfauzi <aziz.fauzi1811@gmail.com>
2024-11-17 12:16:58 +0700

    first commit dev

--- app/src/main/java/com/example/monev/MainActivity.kt
+++ app/src/main/java/com/example/monev/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import com.example.monev.ui.navigation.Navigation
+import com.example.monev.ui.screens.home.HomeScreen
 import com.example.monev.ui.theme.MonevTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MonevTheme {
-                Navigation(modifier = Modifier.fillMaxSize())
+                HomeScreen()
             }
         }
     }
--- app/src/main/java/com/example/monev/ui/navigation/Destination.kt
+++
@@ -1,7 +0,0 @@
-package com.example.monev.ui.navigation
-
-sealed class Destinations (val route: String) {
-    data object HomeScreen: Destinations("HomeScreen")
-    data object AccountScreen: Destinations("AccountScreen")
-    data object SettingScreen: Destinations("SettingScreen")
-}
